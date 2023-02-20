d44043700aeb5f2edc2e72018998ffb5e3966bec
Hung <hunglnhe161003@fpt.edu.vn>
2023-02-21 01:17:35 +0700

    t di duoc den day la xa roi (add menu profile)

--- app/src/main/java/com/example/demodatabase/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/demodatabase/fragments/ProfileFragment.java
@@ -1,70 +1,63 @@
 package com.example.demodatabase.fragments;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
+import com.bumptech.glide.Glide;
 import com.example.demodatabase.R;
-import com.example.demodatabase.databinding.ActivityMainBinding;
+import com.example.demodatabase.databinding.FragmentProfileBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ProfileFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-    ActivityMainBinding activityMainBinding;
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    TextView textViewUsername;
+    ImageView imageViewProfileImage;
+    FirebaseUser currentUser;
+    BottomNavigationView bottomNavigationView;
 
     public ProfileFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    void init(View view) {
+        textViewUsername = view.findViewById(R.id.tv_username);
+        imageViewProfileImage = view.findViewById(R.id.iv_profileImage);
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        bottomNavigationView = view.findViewById(R.id.bottomNavProfile);
+
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    void initData() {
+        if(currentUser == null){
+            return;
         }
+        String username = currentUser.getDisplayName();
+        Uri photoURL = currentUser.getPhotoUrl();
+        Glide.with(this).load(photoURL).error(R.drawable.ic_baseline_alarm_add_24)
+                .into(imageViewProfileImage);
+        textViewUsername.setText(username);
+
+    }
+
+    void bindingAction() {
         ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
-        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         replaceFragment(new ProfileSetFragment());
-        activityMainBinding.bottomNavigation.setOnItemSelectedListener(item -> {
-            switch (item.getItemId()){
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
                 case R.id.profileSets:
                     replaceFragment(new ProfileSetFragment());
                     break;
@@ -77,25 +70,28 @@ public class ProfileFragment extends Fragment {
                     replaceFragment(new ProfileFolderFragment());
                     break;
             }
-
-            return  true;
+            return true;
         });
-
-
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
-
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        init(view);
+        initData();
+        bindingAction();
+        return view;
     }
 
-    private void replaceFragment(Fragment fragment){
-        FragmentManager fragmentManager = getFragmentManager();
+
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.switchMenuProfile,fragment);
-        fragmentTransaction.commit();
+        fragmentTransaction.replace(R.id.switchMenuProfile, fragment).commit();
+
     }
 }
