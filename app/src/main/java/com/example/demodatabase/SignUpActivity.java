911e8dd905b2e40fb11e495c79533dfa8cc3fef0
Hai Tran <haitlhe161311@fpt.edu.vn>
2023-03-02 23:04:34 +0700

    Sign up Layout

--- app/src/main/java/com/example/demodatabase/SignUpActivity.java
+++ app/src/main/java/com/example/demodatabase/SignUpActivity.java
@@ -44,6 +44,7 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        getSupportActionBar().hide();
         init();
         bindingAction();
     }
